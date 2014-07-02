cb7293c16028a4b60f5b3bff93ddc6072dd0140f
BenRuns <benthomaspatterson@gmail.com>
2014-07-02 07:39:56 -0700

    removed heroku addons

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Adding todo items " do
 	let!(:todo_list) {TodoList.create(title: "Grocery  list", description: "Groceries")}
+	
 
 	it "is successful with valid content" do
 		visit_todo_list(todo_list)
